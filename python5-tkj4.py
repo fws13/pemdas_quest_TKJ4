a6d43d804754d3412eca0d6445ee65519d57191a
fws13 <112526448+fws13@users.noreply.github.com>
2022-09-04 18:23:16 -0700

    Add files via upload

---
+++ python5-tkj4.py
@@ -0,0 +1,20 @@
+# konversi dolar ke rupiah  dolar * rupiiah
+
+dolar = 1
+rupiah = 14.895
+idr = dolar * rupiah
+print("1 dolar jika di konversikan kerupiahkan sebesar", idr)
+
+# konversi uero ke yen uero * yen
+
+euro = 1
+yen = 139.27
+jpy = euro * yen
+print("1 euro jika di konversikan ke yen sebesar", jpy)
+
+# konversi won ke rupiah won* rupiah
+
+won = 1
+rupiah = 10.92
+wr = won * rupiah
+print("1 won jika di kkonveriskan ke rupiah sebesar", wr)
