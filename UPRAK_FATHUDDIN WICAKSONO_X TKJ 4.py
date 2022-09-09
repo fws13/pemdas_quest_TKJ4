ea4a612a0b90d0f4bb4493be5be9a06381772367
fws13 <112526448+fws13@users.noreply.github.com>
2022-09-09 00:54:17 -0700

    Add files via upload

---
+++ UPRAK_FATHUDDIN WICAKSONO_X TKJ 4.py
@@ -0,0 +1,38 @@
+# rumus volume balok panjang * lebar * tinggi
+
+panjang = 5
+lebar = 10
+tinggi = 15
+volume_b = panjang * lebar * tinggi
+print("volume balok adalah", volume_b )
+
+# rumus volume kubus sisi * sisi * sisi
+
+sisi1 = 5
+sisi2 = 5
+sisi3 = 5
+
+volume_k = sisi1 * sisi2 * sisi3
+print("volume kubus adalah", volume_k)
+
+# rumus luas  limas 1/3 * luas alas * tinggi
+
+a = 6
+t = 12
+luas_l = a * t /3
+print("luas permukaan limas adalah", luas_l)
+
+# rumus luas tabung 22/7 * r * t
+
+r = 7
+r = 7
+t = 14
+luas_t = r * r * t /7
+print("luas tabung adalah", luas_t)
+
+# celcius to reamur celcius * 4/5
+
+c = 20
+r = 4/5
+suhu = 40 * 4/5
+print("celcius to reamur adalah", suhu)
