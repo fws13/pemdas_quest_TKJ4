88820189fa584f7eaa477f5d259c346b8dce899f
fws13 <112526448+fws13@users.noreply.github.com>
2022-09-12 19:57:52 -0700

    Add files via upload

---
+++ python9-tkj4.py
@@ -0,0 +1,9 @@
+nama_depan = input("masukkan nama depan")
+nama_belakang = input("masukkan nama belakang")
+
+print("\nama anda adalah", nama_depan, nama_belakang)
+
+celcius = int(input("masukkan suhu celcius"))
+reamur = celcius * 4/5
+
+print(celcius, "celcius=", reamur, "reammur")
