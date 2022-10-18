c31ce982f7790b38e982d8cf5860267b0bf69d2f
fws13 <112526448+fws13@users.noreply.github.com>
2022-10-17 19:52:34 -0700

    Add files via upload

---
+++ python11-tkj4.py
@@ -0,0 +1,18 @@
+#general
+gender = ("laki-laki","perempuan")
+
+#menggunakan fungsi tuuple, tuple()
+tuple_name = tuple(["jaya baya", "leborn james"])
+
+gabungan = gender + tuple_name
+print(gabungan)
+
+#buatlah 3  koleksi tuple dengan minimal 4 data disetiap koleksi
+nama_buah = (["jambu", "mangga", "sirsak", "manggis"])
+print(nama_buah)
+
+nama_hewan = (["kucing", "bebek", "anjing", "babi"])
+print(nama_hewan)
+
+nama_sayuran = (["bayem", "kangkung", "sawi", "wortel"])
+print(nama_sayuran)
